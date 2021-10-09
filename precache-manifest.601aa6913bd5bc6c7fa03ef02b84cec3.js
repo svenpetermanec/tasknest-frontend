eba10e17a4c8fedf29c55242b08ead4048d02a48
svenpetermanec <svenpetermanec@users.noreply.github.com>
2021-10-09 14:40:37 +0000

    deploy: 5d9b20f836a0484535aeb4184daa2e28530dab3a

---
+++ precache-manifest.601aa6913bd5bc6c7fa03ef02b84cec3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e98e61735ed2dc9e80ce3a486c608ed",
+    "url": "https://svenpetermanec.github.io/tasknest-frontend/index.html"
+  },
+  {
+    "revision": "63462f17fedf7ca88bd7",
+    "url": "https://svenpetermanec.github.io/tasknest-frontend/static/css/main.87c0382f.chunk.css"
+  },
+  {
+    "revision": "16e7fe275a8a504c9796",
+    "url": "https://svenpetermanec.github.io/tasknest-frontend/static/js/2.fe2c879e.chunk.js"
+  },
+  {
+    "revision": "63462f17fedf7ca88bd7",
+    "url": "https://svenpetermanec.github.io/tasknest-frontend/static/js/main.20698ead.chunk.js"
+  },
+  {
+    "revision": "796471d0e0ea75f9b7ce",
+    "url": "https://svenpetermanec.github.io/tasknest-frontend/static/js/runtime~main.40ebdc5f.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "https://svenpetermanec.github.io/tasknest-frontend/precache-manifest.601aa6913bd5bc6c7fa03ef02b84cec3.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://svenpetermanec.github.io/tasknest-frontend/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
